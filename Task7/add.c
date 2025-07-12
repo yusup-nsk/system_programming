08eaf02d5ca459fd2508639f615c6d4e34b545ee
Yusup Nsk <yusupu@gmail.com>
2025-07-12 22:33:23 +0700

    rm funcs

--- Task7/add.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-void add(int num1, int num2) {
-  printf("\n%d + %d = %d\n", num1, num2, num1 + num2);
-}
--- Task7/div.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void div(int num1, int num2) {
-  if (num2) {
-    printf("\n%d / %d = %d\n", num1, num2, num1 / num2);
-  } else {
-    printf("Второе число не должно быть нулем\n");
-  }
-}
