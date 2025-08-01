5cc0dd237cfed4c0a31c0a17092d1e3a1bee7c9c
Yusup Nsk <yusupu@gmail.com>
2025-08-01 17:34:48 +0700

    works

--- Task11/quest3.c
+++ Task11/quest3.c
@@ -9,66 +9,61 @@
 #include <time.h>
 #include <unistd.h>
 
-#define START_VALUE 5000
+#define START_VALUE 500
 #define LOADER_VALUE 300
 #define CONSUMER_NEEDS 5500
 #define N_SHOPS 5
 #define N_CONSUMERS 3
 #define SLEEP_CONSUMER 2
 #define SLEEP_LOADER 1
-// #define NO_COLOR printf("\033[0m")
-// #define SET_COLOR_32 printf("\033[1;32m")
-// #define SET_COLOR_33 printf("\033[1;33m")
-// #define SET_COLOR_34 printf("\033[1;34m")
 
-
+#define WHITE "\033[0m"
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define VIOLET "\033[1;35m"
 
 pthread_mutex_t mute_shop[N_SHOPS] = PTHREAD_MUTEX_INITIALIZER;
 int shop[N_SHOPS];
 int needs[N_CONSUMERS];
-
+char colour[N_CONSUMERS][12] = {GREEN, YELLOW, BLUE};
 
 void fill_shops_in_start(int shop[N_SHOPS]);
 void fill_consumers_needs(int needs[N_CONSUMERS]);
-void show(int shop[N_SHOPS], int needs[N_CONSUMERS]);
 void make_next_shop_index(int consumer_index, int* shop_index);
-void set_color(unsigned color_num);
 void* consumers_behavior(void* arg);
-
+void* loader_behavior(void* arg);
 
 int main() {
-  srand(time(NULL));
   int* status;
-  //   int shop[N_SHOPS];
-  //   int needs[N_CONSUMERS];
   fill_shops_in_start(shop);
   fill_consumers_needs(needs);
   putchar('\n');
   int var[N_CONSUMERS];
-
-   pthread_t consumer[N_CONSUMERS];
-//    pthread_t loader;
+  pthread_t consumer[N_CONSUMERS];
+  pthread_t loader;
 
   for (int i = 0; i < N_CONSUMERS; ++i) {
     var[i] = i;
     pthread_create(&consumer[i], NULL, consumers_behavior, (int**)&var[i]);
   }
-
+  int repeat_loading = 1;
+  pthread_create(&loader, NULL, loader_behavior, (void*)&repeat_loading);
   for (int i = 0; i < N_CONSUMERS; ++i) {
     pthread_join(consumer[i], (void**)&status);
+    printf("%sConsumer \033[5m#%i finished his thread%s\n", colour[i], i,
+           WHITE);
   }
-
-  // show(shop, needs);
-
+  repeat_loading = 0;
+  pthread_join(loader, (void**)&status);
+  printf("%sLoader finished work\n", VIOLET);
   int is_any_needs = 0;
-  for(int j=0; j<N_CONSUMERS; j++){
+  for (int j = 0; j < N_CONSUMERS; j++) {
     is_any_needs = is_any_needs || needs[j];
   }
-  if (!is_any_needs){
-    printf("\nEACH NEEDS SATISFIED!!!\n");
-  }
-
-
+  if (!is_any_needs) {
+    printf("\033[1;36m\nEACH NEEDS SATISFIED!!!\n");
+  } else printf("\033[5mSOMETHING GONE WRONG!\n");
   exit(EXIT_SUCCESS);
 }
 
@@ -88,25 +83,7 @@ void fill_consumers_needs(int needs[N_CONSUMERS]) {
   for (int i = 0; i < N_CONSUMERS; ++i) {
     needs[i] = CONSUMER_NEEDS - (CONSUMER_NEEDS * 0.3) +
                (CONSUMER_NEEDS * 0.3 * 2 * rand() / RAND_MAX);
-    printf("Consumer #%d: at start needs %d products\n", i, needs[i]);
   }
-}
-
-void show(int shop[N_SHOPS], int needs[N_CONSUMERS]) {
-  putchar('\n');
-  putchar('\n');
-  putchar('\n');
-  printf("LOADER is ...\n");
-  for (int i = 0; i < N_SHOPS; ++i) {
-    printf("shop %d:%d      ", i + 1, shop[i]);
-  }
-  putchar('\n');
-  putchar('\n');
-  for (int i = 0; i < N_CONSUMERS; ++i) {
-    printf("  %d:::%d", i + 1, needs[i]);
-  }
-  putchar('\n');
-  putchar('\n');
 }
 
 void make_next_shop_index(int consumer_index, int* shop_index) {
@@ -117,49 +94,55 @@ void make_next_shop_index(int consumer_index, int* shop_index) {
   }
 }
 
-
-void set_color(unsigned color_num){
-    char str[20];
-    sprintf(str, "\033[%dm", color_num) ;
-    printf("%s", str);
-}
-
-
 void* consumers_behavior(void* arg) {
   int cons_index = *(int*)arg;
-  set_color(cons_index +32);
-  printf("Consumer #%d starts, needs %d\n", cons_index, needs[cons_index]);
-//   pthread_t this_tid = pthread_self();
+  printf("%sConsumer #%d starts, needs %d\n",WHITE, cons_index, needs[cons_index]);
   int shop_index = (cons_index * N_SHOPS / 2) % N_SHOPS;
   int did_purchase = 0;
-//   int visited_shops = 0;
   int has_needs = 1;
   for (; has_needs; make_next_shop_index(cons_index, &shop_index)) {
     pthread_mutex_lock(&mute_shop[shop_index]);
-    printf("Consumer #%d come to shop #%d. Needs = %d\n", cons_index,
-           shop_index, needs[cons_index]);
+    printf("%sConsumer #%d come to shop #%d. Needs = %d\n", colour[cons_index],
+           cons_index, shop_index, needs[cons_index]);
     if (shop[shop_index]) {
       if (shop[shop_index] >= needs[cons_index]) {
         shop[shop_index] -= needs[cons_index];
         needs[cons_index] = 0;
         has_needs = 0;
-        printf("Consumer #%d: Satisfied the needs!!! \n", cons_index);
+        printf("\n\t\t\t%sConsumer #%d: Satisfied the needs!!! \n\n",colour[cons_index], cons_index);
       } else {
         needs[cons_index] -= shop[shop_index];
         shop[shop_index] = 0;
         did_purchase = 1;
-        printf("Consumer #%d: Did purchase....\n", cons_index);
+        printf("%sConsumer #%d: Did purchase....\n", colour[cons_index], cons_index);
       }
+    }
+    if (did_purchase && has_needs) {
+      printf(
+          "%sConsumer #%d: After shopping in shop #%d, I'm going to sleep with "
+          "needs = %d\n", colour[cons_index],
+          cons_index, shop_index, needs[cons_index]);
     }
     pthread_mutex_unlock(&mute_shop[shop_index]);
     if (did_purchase && has_needs) {
-      printf(
-          "Consumer #%d: After shopping in shop #%d, I'm going to sleep with "
-          "needs = %d\n",
-          cons_index, shop_index, needs[cons_index]);
       sleep(SLEEP_CONSUMER);
     }
   }
-  set_color(0);
-  return needs;
+  return NULL;
+}
+
+void* loader_behavior(void* arg) {
+  int* repeat = (int*)arg;
+  int shop_index = 0;
+  while (*repeat) {
+    pthread_mutex_lock(&mute_shop[shop_index]);
+    shop[shop_index] += LOADER_VALUE;
+    printf(
+        "%sLoader brings products to shop #%d and goes to sleep\n", VIOLET,
+        shop_index);
+    pthread_mutex_unlock(&mute_shop[shop_index]);
+    shop_index = (shop_index + 1) % N_SHOPS;
+    sleep(SLEEP_LOADER);
+  }
+  return NULL;
 }
