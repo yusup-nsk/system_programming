786df9d861c1c3af43366ee62b6a87dd777257b6
Yusup Nsk <yusupu@gmail.com>
2025-08-05 23:57:52 +0700

    function get_token from original array of strings

--- Task12/Quest3/quest3.c
+++ Task12/Quest3/quest3.c
@@ -10,8 +10,42 @@
 #define MAX_LINE 512
 #define SZ 32
 
+void output_arr(char **arr, unsigned count);
 int input_arr(char ***arr, unsigned *count);
 void free_arr(char **arr, unsigned count);
+int get_token(char **arr, char ***token, unsigned *index_in_arr) {
+  if (!(arr)) {
+    return -1;
+  }
+
+  int count = 0;
+  // output_arr(arr, 2);
+  char **ptr = arr + *index_in_arr;
+  for (; ptr && *ptr; ptr++, count++) {
+    if (*ptr)
+      printf("*ptr=%s\n", *ptr);
+    else
+      printf("*ptr=%s\n", "Null");
+    if (strncmp(*ptr, "|", 2) == 0) {
+      break;
+    }
+  }
+  if (NULL == *ptr || 0 == count) {
+    return 0;
+  }
+
+  count++;
+  *token = (char **)malloc((count) * sizeof(char *));
+  for (int i = 0; i < count - 1; i++) {
+    unsigned size = strlen(arr[*index_in_arr + i]) + 1;
+    (*token)[i] = (char *)malloc(size * sizeof(char));
+    memcpy((*token)[i], arr[*index_in_arr + i], size);
+  }
+  (*token)[count - 1] = NULL;
+  output_arr(*token, count);
+  *index_in_arr += count;
+  return count;
+}
 
 int main() {
   unsigned count;
@@ -21,14 +55,37 @@ int main() {
     int res = input_arr(&arr, &count);
     if (-1 == res) {
       printf("\nInput failed\n\n");
+      continue;
+    } else if (0 == res) {
+      continue;
     }
     if (strncmp(arr[0], "exit", 5)) {
-      pid_t pid = fork();
+      // pid_t pid = fork();
+      int pid = 0;
       if (0 == pid) {
-        execvp(arr[0], arr);
+        char **token = NULL;
+        unsigned index_in_arr = 0;
+        int cnt_token = get_token(arr, &token, &index_in_arr);
+        printf("cnt_token = %d; pointer token = %p\n", cnt_token,
+               (void *)token);
+        if (-1 == cnt_token) {
+        } else if (0 == cnt_token) {
+          token = arr;
+          // execve(arr[0], arr);
+        } else {
+          for (int i = 0; i < cnt_token; ++i) {
+            if (token[i])
+              printf("token[%d]:<%s>\n", i, token[i]);
+            else
+              printf("token[%d]:<%s>\n", i, "NUL");
+          }
+          //  execvp(token[0], token);
+        }
+
+        free_arr(token, cnt_token);
       }
-      wait(NULL);
-      kill(pid, SIGTERM);
+      // wait(NULL);
+      // kill(pid, SIGTERM);
     } else
       repeat = 0;
 
@@ -46,9 +103,9 @@ int input_arr(char ***arr, unsigned *count) {
   printf("Input command: ");
   char line[MAX_LINE];
   fgets(line, MAX_LINE, stdin);
-  if (0 == line[0]) {
+  if (0 == line[0] || '\n' == line[0]) {
     *count = 0;
-    *arr = NULL;
+    // *arr = NULL;
     return 0;
   } else
     line[MAX_LINE - 1] = 0;
@@ -85,5 +142,16 @@ void free_arr(char **arr, unsigned count) {
       }
     }
     free(arr);
+  }
+}
+
+void output_arr(char **arr, unsigned count) {
+  if (arr) {
+    for (unsigned i = 0; i < count; ++i) {
+      if (arr[i]) {
+        printf("%u:  %s\n", i, arr[i]);
+      } else
+        printf("%u:  %s\n", i, "NULL");
+    }
   }
 }
