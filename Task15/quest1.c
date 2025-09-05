10e2216ce075af33c6ab39e4db83f66409867e98
Yusup Nsk <yusupu@gmail.com>
2025-09-06 03:52:08 +0700

    fin1

--- Task15/quest1.c
+++ Task15/quest1.c
@@ -1,5 +1,4 @@
 // make disposition for SIGUSR1
-#include <errno.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,22 +6,23 @@
 #include <unistd.h>
 
 int g_repeats = 3;
-void sig_handler(int sig_num, siginfo_t *info, void *args) {
+void sig_handler(int sig_num, siginfo_t *info, void *context) {
   if (SIGUSR1 == sig_num) {
     printf("Signal SIGUSR1 achieved\n");
   } else {
     printf("Signal  #%d achieved\n \n", sig_num);
   }
   if (g_repeats == 1) {
-    printf("Arguments of function sa_sighandler are:\nsiginfo_t is a structure "
-           "of:\n");
+    printf("\nFirst argument is signal number\n");
+    printf("Second arguments of function sa_sighandler is 'siginfo_t info' "
+           "wich is a structure "
+           "with fields:\n");
     printf("  si_signo = %d\n", info->si_signo);
     printf("  si_errno = %d\n", info->si_errno);
     printf("  si_code = %d\n", info->si_code);
-    // printf("  si_trapno = %d\n", info->si_trapno);
     printf("  si_pid = %d\n", info->si_pid);
     printf("  si_uid = %d\n", info->si_uid);
-    printf("  si_status = %d\n.....\n", info->si_status);
+    printf("  si_status = %d\n  .....\n", info->si_status);
     printf("  si_int = %d\n", info->si_int);
     printf("  si_overrun = %d\n", info->si_overrun);
     printf("  si_addr = %p\n", info->si_addr);
@@ -34,9 +34,10 @@ void sig_handler(int sig_num, siginfo_t *info, void *args) {
     printf("  si_syscall = %d\n", info->si_syscall);
     printf("  si_arch = %u\n", info->si_arch);
 
-    ucontext_t ucontext = *(ucontext_t *)args;
-    printf("\nucontext_t is a struct of:\n");
-    printf("  pointer uc_link = %p\n", ucontext.uc_link);
+    ucontext_t ucontext = *(ucontext_t *)context;
+    printf(
+        "\nThird argument is 'ucontext_t * ucontext' wich is a struct of:\n");
+    printf("  pointer uc_link = %p\n", (void *)ucontext.uc_link);
     printf("  stack_t uc_stack is a struct of\n");
     printf("    void *ss_p = %p\n", ucontext.uc_stack.ss_sp);
     printf("    size_t ss_size = %lu\n", ucontext.uc_stack.ss_size);
@@ -48,7 +49,6 @@ void sig_handler(int sig_num, siginfo_t *info, void *args) {
 
 int main() {
   struct sigaction handler;
-  struct sigaction oldact;
   sigset_t set;
   int ret;
 
@@ -59,17 +59,12 @@ int main() {
   handler.sa_mask = set;
   handler.sa_flags = SA_SIGINFO | SA_RESTART;
 
-  ret = sigaction(SIGUSR1, &handler, &oldact);
+  ret = sigaction(SIGUSR1, &handler, NULL);
   if (ret < 0) {
     perror("can't set signal handler!\n");
     exit(EXIT_FAILURE);
   }
-  printf("oldact is a struct sigaction of:\n");
-  printf("  pointer to function sa_action = %p\n", oldact.sa_sigaction);
-  sigset_t samask = oldact.sa_mask;
-  unsigned int *ptr = (int *)&samask;
-  printf("  sigset_t sa_mask = %#x | %#x\n", *ptr, *(ptr + 1));
-  printf("  sigset_t sa_flags = %lld\n", (long long)oldact.sa_flags);
+
   while (g_repeats) {
     sleep(1);
   }
--- Task15/quest3.c
+++ Task15/quest3.c
@@ -1,70 +1,31 @@
-// sigwait()
-#include <errno.h>
+// sigwait() for the event loop
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ucontext.h>
 #include <unistd.h>
 
-int g_repeats = 3;
-void signal_handler(int sig_num) {
-  if (SIGUSR2 == sig_num) {
-    printf("\nSignal SIGUSR2 achieved!\n\n");
-  }
-  g_repeats--;
-}
-
 int main() {
-  struct sigaction handler;
-  struct sigaction oldact;
-  sigset_t set, set2, set3;
+  sigset_t set;
   int ret;
   int sig_num;
 
   sigemptyset(&set);
   sigaddset(&set, SIGUSR1);
 
-  //  handler.sa_sigaction = sig_handler;
-  handler.sa_handler = signal_handler;
-  handler.sa_mask = set;
-  // handler.sa_flags = SA_SIGINFO | SA_RESTART;
-
-  // ret = sigaction(SIGUSR1, &handler, &oldact);
   ret = sigprocmask(SIG_BLOCK, &set, NULL);
   if (ret < 0) {
     perror("can't set blocking signal SUGUSR1!\n");
     exit(EXIT_FAILURE);
   }
-  ret = sigaction(SIGUSR2, &handler, &oldact);
-  for (int i = 0; i <= 64; i++) {
-    if (sigismember(&set, i)) {
-      printf("%d is member\n", i);
-    }
-  }
-  sigpending(&set3);
-  for (int i = 0; i <= 64; i++) {
-    if (sigismember(&set, i)) {
-      printf("%d is member of pending set\n", i);
-    }
-  }
+
   while (1) {
-    // sleep(3);
-    if (0 == g_repeats) {
-      break;
-    } 
-    printf("\nWaiting signal SIGUSR1\n");
-    // sigwait(&set, &sig_num);
-    // if (SIGUSR1 == sig_num) {
-    //   printf("Achieved blocked signal SIGUSR1.\n");
-    // }
-    printf("Doing instruction inside loop\ng_repeats = %d\n", g_repeats);
+    printf("\nWaiting for signal SIGUSR1\n");
+    sigwait(&set, &sig_num);
+    if (SIGUSR1 == sig_num) {
+      printf("Achieved blocked signal SIGUSR1.\n");
+    }
+    printf("Doing instruction inside loop\n");
   }
 
-  sigpending(&set3);
-  for (int i = 0; i <= 64; i++) {
-    if (sigismember(&set, i)) {
-      printf("%d is member of pending set\n", i);
-    }
-  }
   exit(EXIT_SUCCESS);
 }
